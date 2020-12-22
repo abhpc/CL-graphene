209cb236a1be6b9ad54c4aa3235eddd43d84a632
abhpc <37471375+abhpc@users.noreply.github.com>
2020-12-22 22:47:14 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -21,20 +21,24 @@ YL=`head $cfgfile | grep "H0(2,2)"|awk '{print $3}'|cut -d '.' -f1`
 # define the width of the line
 linewidth=3
 
-#
-XLhi=$[$XL/100*50-50]
+# define the interval distance between lines
+interval=50
+Doubleinterval=$[$interval*2]
+
+# The maximum and minimum X
+XLhi=$[$XL/$Doubleinterval*$interval-$interval]
 XLlo=-$XLhi
 
-YLhi=$[$YL/100*50-50]
+# The maximum and minimum Y
+YLhi=$[$YL/$Doubleinterval*$interval-$interval]
 YLlo=-$YLhi
 
 
 # load the ovito 3.0.0 according to your situation.
 module load ovito/3.0.0dev
 
-
-echo "When the lines are along x-axis, obtain the grain numbers:"
 # When the lines are along x-axis, obtain the grain numbers
+echo "When the lines are along x-axis, obtain the grain numbers:"
 for i in `seq $YLlo 50 $YLhi`
 do
   ovitos linecut-cfg.py $cfgfile Y $i $[$i+$linewidth]
@@ -42,8 +46,8 @@ done
 
 echo ""
 
-echo "When the lines are along y-axis, obtain the grain numbers:"
 # When the lines are along y-axis, obtain the grain numbers
+echo "When the lines are along y-axis, obtain the grain numbers:"
 for i in `seq $XLlo 50 $XLhi`
 do
   ovitos linecut-cfg.py $cfgfile X $i $[$i+$linewidth]
