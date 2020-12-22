b1d1b6c3f1dba5e37d0bb10afe13da51afb50336
abhpc <37471375+abhpc@users.noreply.github.com>
2020-12-22 22:32:31 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -25,11 +25,25 @@ linewidth=3
 XLhi=$[$XL/100*50-50]
 XLlo=-$XLhi
 
+YLhi=$[$YL/100*50-50]
+YLlo=-$YLhi
+
+
 # load the ovito 3.0.0 according to your situation.
 module load ovito/3.0.0dev
 
 
+echo "When the lines are along x-axis, obtain the grain numbers:"
 # When the lines are along x-axis, obtain the grain numbers
+for i in `seq $YLlo 50 $YLhi`
+do
+  ovitos linecut-cfg.py $cfgfile Y $i $[$i+$linewidth]
+done
+
+echo ""
+
+echo "When the lines are along y-axis, obtain the grain numbers:"
+# When the lines are along y-axis, obtain the grain numbers
 for i in `seq $XLlo 50 $XLhi`
 do
   ovitos linecut-cfg.py $cfgfile X $i $[$i+$linewidth]
